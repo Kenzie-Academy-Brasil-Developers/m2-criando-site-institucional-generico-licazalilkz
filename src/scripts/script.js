e9e9878a74655e04d2fac0c283c9ed46a41b5cde
licazalilkz <licazalilkz>
2023-04-06 14:28:16 -0300

    feito

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,27 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+
+function handleModal(){
+    const button = document.querySelector('.header__button');
+    const modalController = document.querySelector('.modal__controller');
+    const buttonDoubt = document.querySelector('.button__doubt');
+
+    button.addEventListener('click', ()=>{
+        modalController.showModal();
+        closeModal();
+    })
+    buttonDoubt.addEventListener('click', ()=>{
+        modalController.showModal();
+        closeModal();
+    })
+}
+
+function closeModal(){
+    const button = document.querySelector('.modal__close');
+    const modalController = document.querySelector('.modal__controller');
+
+    button.addEventListener('click', () => {
+        modalController.close();
+    })
+}
+
+handleModal();
